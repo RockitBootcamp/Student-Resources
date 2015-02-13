ea7eb9d366a380af7d47583a773cbb839b6b9ace
Brad Westfall <brad@azpixels.com>
2015-02-13 16:39:56 -0700

    refactor

--- PHP/17 Templating/practice/complex_string_replacement.php
+++ PHP/17 Templating/practice/complex_string_replacement.php
@@ -5,15 +5,10 @@ class View {
 	
 	// Fill Template
 	public static function fill($record, $template) {
-		$search_keys = array_keys($record);
-		array_walk($search_keys, ['self', 'fixKeys']);
-		$values = array_values($record);
-		return str_replace($search_keys, $values, $template);
-	}
-
-	// Callback to fix keys with mustashes
-	private static function fixKeys(&$search) {
-		$search = '{{' . $search . '}}';
+		foreach ($record as $key => $field) {
+			$template = str_replace('{{' . $key . '}}', $field, $template);
+		}
+		return $template;
 	}
 
 }
