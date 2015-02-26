4a531d13baaf6e56691be40d873103722313b379
Brad Westfall <brad@azpixels.com>
2015-02-26 16:06:28 -0700

    organize

--- PHP/Laravel/Models/Models.md
+++ PHP/Laravel/Models/Models.md
@@ -1,75 +1,10 @@
-# Laravel Models and Collections
+# Models and Collection Class
 
 ## Installation
 
-1. Create two new folders: `app/Models` and `app/Library`
+1. Create a new folder: `app/Models`
 1. Place the `Model.php` file in the `app/Models` folder
 1. Place the `Collection.php` file in the `app/Models` folder
-1. Place the `Sql.php` file in the `app/Library` folder
-
-Edit your PDO configurations settings by going to `/config/database.php` and adding this line to the "connections => mysql" array:
-
-```php
-'options' => [PDO::ATTR_EMULATE_PREPARES => TRUE]
-```
-
-> Without this option you can expect to get parameter binding issues especially for `UPDATE` statements
-
-## SQL Class
-
-The `Sql.php` class is a utility class that will help you write `INSERT` and `UPDATE` SQL statements. To use the class be sure to `use App\Library\Sql;` where you plan on calling the methods of `Sql`.
-
-### Insert
-
-The following code will perform an `INSERT` statement and will return to you the ID that was created:
-
-```php
-$data = [
-		'first_name'   => 'William',
-		'last_name'    => 'Clinton',
-		'email'        => 'honestguy@yahoo.com',
-		'password'     => 'monicalove'
-	];
-
-// Insert the data into the "user" table
-$id = Sql::insert('user', $data);
-```
-
-Passing a third argument into `insert()` will let the insert statement know which fields to "ignore" for the purposes of binding parameters:
-
-```php
-$data = [
-		'first_name'      => 'William',
-		'last_name'       => 'Clinton',
-		'email'           => 'honestguy@yahoo.com',
-		'password'        => 'monicalove',
-		'datetime_added'  => 'NOW()'
-	];
-
-// Insert the data into the "user" table
-$id = Sql::insert('user', $data, ['datetime_added']);
-```
-
-In the above example, we want the `datetime_added` column of our table to get MySQL's `NOW()` function applied to it. If we didn't pass the ignore array as the third argument, it would be like saying we wanted the string of "NOW()" to be inserted.
-
-> Note that in both examples above, we didn't have to pass an ID into `insert()` because we're assuming this table has an automatic incrementing ID.
-
-### Update
-
-The update statements work just like the insert one. Simply call `Sql::update()` as follows:
-
-```php
-$data = [
-		'user_id'      => 1,
-		'first_name'   => 'Bill'
-	];
-
-// Update the data in the "user" table
-Sql::update('user', $data, "WHERE user_id = :user_id");
-```
-
-Notice the third parameter to update is an SQL "WHERE" clause. Be sure to include the parameterized value (`:user_id` in this case) in the `$data`. Also note that the data is only updating the user's name and does not have to contain each field from the database, only the ones you want updated.
-
 
 ## Models
 
---
+++ PHP/Laravel/Models/Sql.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Library;
+use DB;
+
+class Sql {
+
+	/****************************************
+	  Insert
+	*****************************************/
+
+	/**
+	 * Execute Standard INSERT statement
+	 */
+	public static function insert($table_name, $sql_values, $ignore = []) {
+		$sql = self::makeInsertStatement($table_name, $sql_values, $ignore);
+		$sql_values = self::removeIgnores($sql_values, $ignore);
+		DB::insert($sql, $sql_values);
+		return DB::getPdo()->lastInsertId();
+	}
+
+	/**
+	 * Execute INSERT statement with ON DUPLICATE KEY UPDATE added
+	 */
+	public static function insertUpdate($table_name, $sql_values, $ignore = []) {
+		$sql = self::makeInsertStatement($table_name, $sql_values, $ignore) . "\r\nON DUPLICATE KEY UPDATE " . self::makeUpdateValues($sql_values, $ignore);
+		$sql_values = self::removeIgnores($sql_values, $ignore);
+		return DB::insert($sql, $sql_values);
+	}
+
+	/**
+	 * Create Standard INSERT statement
+	 */
+	private static function makeInsertStatement($table_name, $sql_values, $ignore = []) {
+		
+		// Create SQL statement
+		$sql = "INSERT INTO `{$table_name}` (`" . implode('`, `', array_keys($sql_values)) . "`) VALUES (:" . implode(', :', array_keys($sql_values)) . ")";
+		
+		// Replace prepared placeholders with original values if needed
+		foreach ($ignore as $key) {
+			$sql = str_replace(':' . $key, $sql_values[$key], $sql);
+		}
+		
+		// Return SQL
+		return $sql;
+	}
+
+	/****************************************
+	  Update
+	*****************************************/
+
+	/**
+	 * Execute Standard UPDATE statement
+	 */
+	public static function update($table_name, $sql_values, $sql_where, $ignore = []) {
+		$sql = "UPDATE `{$table_name}` SET " . self::makeUpdateValues($sql_values, $ignore) . ' ' . $sql_where;
+		$sql_values = self::removeIgnores($sql_values, $ignore);
+		return DB::update($sql, $sql_values);
+	}
+
+	/**
+	 * Create UPDATE key-value pairs
+	 */
+	public static function makeUpdateValues($sql_values, $ignore = []) {
+		
+		// Loop values to make query string in UPDATE format
+		$sql_array = [];
+		foreach ($sql_values as $key => $value) {
+			if (in_array($key, $ignore)) {
+				$sql_array[] = "`{$key}`={$value}";
+			} else {
+				$sql_array[] = "`{$key}`=:{$key}";
+			}
+		}
+		
+		return implode(',', $sql_array);
+	}
+
+	/****************************************
+	  Utilities
+	*****************************************/
+
+	/**
+	 * Removes keys from $sql_values if they match values from $ignore
+	 */
+	private static function removeIgnores($sql_values, $ignore) {
+		return array_diff_key($sql_values, array_flip($ignore));
+	}
+
+	/**
+	 * Make an SQL "WHERE" clause
+	 */
+	public static function where($conditions) {
+		if (empty($conditions)) return;
+		foreach ($conditions as $key => $value) {
+		    $sql_where[] = "`{$key}`=:{$key}";
+		}
+		return "\nWHERE " . implode(' AND ', $sql_where);
+	}
+
+}
