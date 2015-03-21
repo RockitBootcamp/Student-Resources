a24954cad642e1cac4faaf205d78c2c4dd511a8d
Brad Westfall <brad@azpixels.com>
2015-03-20 21:49:21 -0700

    Update Collection.php

--- PHP/Laravel/Setup/Collection.php
+++ PHP/Laravel/Setup/Collection.php
@@ -50,7 +50,7 @@ class Collection {
 	 * Magic To String
 	 */
 	public function __toString() {
-	    return empty($this->models) ? 'Collection has no models' : $this->getArrayDeep();
+	    return empty($this->models) ? 'Collection has no models' : print_r($this->getArrayDeep(), true);
 	}
 
 
