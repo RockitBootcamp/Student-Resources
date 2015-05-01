33de988beeda14296f62b19a207fbb08d9717c4e
Brig Lamoreaux <brig.lamoreaux@apollo.edu>
2015-05-01 09:49:10 -0700

    Add Session example

---
+++ PHP/12 Sessions/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Index.php
+
+session_start();
+
+
+?>
+
+<h1>Index Page</h1>
+<p>Welcome</p>
---
+++ PHP/12 Sessions/Profile.php
@@ -0,0 +1,9 @@
+<?php
+
+// Profile.php
+
+session_start();
+
+?>
+
+<h1>Hello <?php echo $_SESSION["name"] ?></h1>
